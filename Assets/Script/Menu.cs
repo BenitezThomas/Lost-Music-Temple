cec35b6fb463e17ace1a799d920a6f490dfcfa91
Daniela Luiza Duwe Heiderscheidt <danii.bnu@hotmail.com>
2024-06-11 22:51:40 -0300

    Menu and Interactable object

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] int SceneNum; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
