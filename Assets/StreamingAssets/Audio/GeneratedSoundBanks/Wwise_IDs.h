bf127ac73baf9a7c8a00c04865ee3dae868585e8
BenitezThomas <thominelb@gmail.com>
2024-06-13 14:21:46 -0300

    Implement Wwise and Added Footsteps and String Notes

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FOOTSTEPS_WOOD = 2064026589U;
+        static const AkUniqueID PLAY_STRING_NOTE_B = 2668293571U;
+        static const AkUniqueID PLAY_STRING_NOTE_C = 2668293570U;
+        static const AkUniqueID PLAY_STRING_NOTE_D = 2668293573U;
+        static const AkUniqueID PLAY_STRING_NOTE_E = 2668293572U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
