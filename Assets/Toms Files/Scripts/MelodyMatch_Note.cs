93604eee0bce0033607b12cb3e380aef49010a22
BenitezThomas <thominelb@gmail.com>
2024-06-12 22:03:02 -0300

    Added MelodyMatch Puzzle to level 1

---
+++ Assets/Toms Files/Scripts/MelodyMatch_Note.cs
@@ -0,0 +1,46 @@
+// Tom
+//Latest Update: 06/12/2024
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelodyMatch_Note : MonoBehaviour
+{
+    [SerializeField] MusicalNote note;
+    [SerializeField] MelodyMatch_Manager puzzleManager;
+
+    private bool isPlayerNear = false;
+
+    private void PlayNote()
+    {
+        //play note sound
+
+        //Keep Track of the Next Note in the Melody List
+        puzzleManager.CheckIfNoteIsCorrect(note);
+    }
+
+    private void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E)) PlayNote();
+    }
+
+    //Detection of player proximity
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            Debug.Log("collider");
+        }
+    }
+
+    //Player move away detection
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+        }
+    }
+}
