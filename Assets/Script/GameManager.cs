85ce10937289080052c0295709b62803407eac52
BenitezThomas <thominelb@gmail.com>
2024-07-10 19:37:11 -0300

    First submission

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,10 +9,10 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     // List of levels (Scenes)
-    [SerializeField] private List<GameObject> notes;
+    //[SerializeField] private List<GameObject> notes;
 
     // Current level index
-    private int currentNoteIndex = 0;
+    public int currentNoteIndex = 0;
 
     private void Awake()
     {
@@ -26,39 +26,26 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        //LoadGameState();
     }
 
     private void Start()
     {
-        LoadGameState(); 
-
-        // Initialize by deactivating all notes and activating the first one
-        foreach (GameObject note in notes)
-        {
-            note.SetActive(false);
-        }
-
-        if (notes.Count > 0 && currentNoteIndex < notes.Count)
-        {
-            notes[currentNoteIndex].SetActive(true);
-        }
+        //LoadGameState(); 
     }
 
     public void CollectSong(int puzzleLevel)
     {
-        if (puzzleLevel != 0)
+        if (puzzleLevel > 1)
         {
             // Deactivate the current note
-            notes[currentNoteIndex].SetActive(false);
+            //notes[currentNoteIndex].SetActive(false);
 
             // Increment the note index
             currentNoteIndex++;
 
             // If there are more notes, activate the next one
-            if (currentNoteIndex < notes.Count)
-            {
-                notes[currentNoteIndex].SetActive(true);
-            }
         }
 
         // Load the puzzle level
