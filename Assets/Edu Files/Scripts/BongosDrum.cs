090691198e951aa96732cc60191ff63dd1265b89
Chiaratto12 <eduardo.chiaratto05@gmail.com>
2024-07-12 00:47:37 -0300

    Bongo's jump 1.1

--- Assets/Edu Files/Scripts/BongosDrum.cs
+++ Assets/Edu Files/Scripts/BongosDrum.cs
@@ -14,29 +14,25 @@ using UnityEngine.UIElements;
 public class BongosDrum : MonoBehaviour
 {
     public bool isEnable;
-    [Tooltip("The number that will be multiplied when player jumps. The default to work when player jumps in bongo's drum is 2")]
-    public float jumpMultiplier = 2;
-    [Tooltip("The number that calculate if the player fall's velocity is enough to automatically jump. The higher the value, the greater the supported fall height. The default to work when player jumps in bongo's drum is 5")]
-    public float fallFactor = 5;
-    private bool jumpAgain = true;
+    [Tooltip("The number that will be multiplied when player jumps. The default to work when player jumps in bongo's drum is 2.")]
+    public float jumpMultiplier = 2f;
+    [Tooltip("The number that calculate if the player fall's velocity is enough to automatically jump. The higher the value, the greater the supported fall height. The default to work when player jumps with jumpMultiplier is 2 in bongo's drum is 6.5.")]
+    public float fallFactor = 6.5f;
+    [Tooltip("The number from the highest height the player fell. Your FallFactor need to be more than this.")]
+    public float fallMaxVelocity;
 
     private void OnTriggerEnter(Collider other)
     {
         if (isEnable && other.tag == "Player")
         {
             var player = other.GetComponent<CharacterController>();
-            if (player.velocity.y < 0 && player.velocity.y > -fallFactor && jumpAgain && jumpMultiplier > 0)
+            fallMaxVelocity = player.velocity.magnitude;
+            if (player.velocity.magnitude > 0 && player.velocity.magnitude < fallFactor && jumpMultiplier > 0)
             {
                 var playerJump = player.GetComponent<ThirdPersonMovement>();
                 playerJump.JumpHigher(jumpMultiplier);
-                jumpAgain = false;
+                //jumpAgain = false;
             }
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        var player = other.GetComponent<CharacterController>();
-        if (!jumpAgain && player.velocity.y > 0) jumpAgain = true;
     }
 }
