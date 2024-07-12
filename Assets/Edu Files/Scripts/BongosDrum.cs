7ddeb72c6d7cd4f6ec193dfa2a932e1b5f9fce5d
Chiaratto12 <eduardo.chiaratto05@gmail.com>
2024-07-12 02:15:51 -0300

    Bongo's jump 1.2

--- Assets/Edu Files/Scripts/BongosDrum.cs
+++ Assets/Edu Files/Scripts/BongosDrum.cs
@@ -16,9 +16,9 @@ public class BongosDrum : MonoBehaviour
     public bool isEnable;
     [Tooltip("The number that will be multiplied when player jumps. The default to work when player jumps in bongo's drum is 2.")]
     public float jumpMultiplier = 2f;
-    [Tooltip("The number that calculate if the player fall's velocity is enough to automatically jump. The higher the value, the greater the supported fall height. The default to work when player jumps with jumpMultiplier is 2 in bongo's drum is 6.5.")]
-    public float fallFactor = 6.5f;
-    [Tooltip("The number from the highest height the player fell. Your FallFactor need to be more than this.")]
+    [Tooltip("The number that calculate if the player fall's velocity is enough to automatically jump. The higher the value, the greater the supported fall height.")]
+    public float fallFactor = 1;
+    [Tooltip("The number from velocity of the highest height the player fell.")]
     public float fallMaxVelocity;
 
     private void OnTriggerEnter(Collider other)
@@ -27,7 +27,7 @@ public class BongosDrum : MonoBehaviour
         {
             var player = other.GetComponent<CharacterController>();
             fallMaxVelocity = player.velocity.magnitude;
-            if (player.velocity.magnitude > 0 && player.velocity.magnitude < fallFactor && jumpMultiplier > 0)
+            if (player.velocity.magnitude > 0 && player.velocity.magnitude < fallFactor * 6.5f && jumpMultiplier > 0)
             {
                 var playerJump = player.GetComponent<ThirdPersonMovement>();
                 playerJump.JumpHigher(jumpMultiplier);
