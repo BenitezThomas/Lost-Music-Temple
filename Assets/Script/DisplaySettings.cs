626602ec529e62abaf457a2e5cbed525ddc52fe2
Daniela Luiza Duwe Heiderscheidt <74636092+DanielaLDH@users.noreply.github.com>
2024-09-13 01:08:03 -0300

    Update DisplaySettings - playerprefs

--- Assets/Script/DisplaySettings.cs
+++ Assets/Script/DisplaySettings.cs
@@ -21,6 +21,8 @@ public class DisplaySettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        LoadSettings();
+
         resolutions = Screen.resolutions;
         filteredResolutions = new List<Resolution>();
 
@@ -82,32 +84,74 @@ public class DisplaySettings : MonoBehaviour
         vsyncToggle.isOn = QualitySettings.vSyncCount > 0;
         vsyncToggle.onValueChanged.AddListener(SetVSync);
     }
-    
+
     public void SetResolution(int resolutionIndex)
     {
         Resolution resolution = filteredResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        SaveSettings();
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        SaveSettings();
     }
 
     public void SetQuality(int qualityIndex)
     {
-        Debug.Log(qualityIndex);
         QualitySettings.SetQualityLevel(qualityIndex);
+        SaveSettings();
         UpdateVSyncToggle();
     }
 
     public void SetVSync(bool isVSync)
     {
         QualitySettings.vSyncCount = isVSync ? 1 : 0;
+        SaveSettings();
     }
 
     private void UpdateVSyncToggle()
     {
         vsyncToggle.isOn = QualitySettings.vSyncCount > 0;
+    }
+
+
+    private void LoadSettings()
+    {
+
+        int savedResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", currentResolutionIndex);
+        SetResolution(savedResolutionIndex);
+        resolutionDropdown.value = savedResolutionIndex;
+
+        bool savedFullscreen = PlayerPrefs.GetInt("Fullscreen", Screen.fullScreen ? 1 : 0) == 1;
+        Screen.fullScreen = savedFullscreen;
+        fullscreenToggle.isOn = savedFullscreen;
+
+        int savedQualityLevel = PlayerPrefs.GetInt("QualityLevel", QualitySettings.GetQualityLevel());
+        QualitySettings.SetQualityLevel(savedQualityLevel);
+        qualityDropdown.value = savedQualityLevel;
+
+        bool savedVSync = PlayerPrefs.GetInt("VSync", QualitySettings.vSyncCount > 0 ? 1 : 0) == 1;
+        QualitySettings.vSyncCount = savedVSync ? 1 : 0;
+        vsyncToggle.isOn = savedVSync;
+    }
+
+    private void InitializeQualitySettings()
+    {
+        qualityDropdown.ClearOptions();
+        List<string> qualityOptions = new List<string>(QualitySettings.names);
+        qualityDropdown.AddOptions(qualityOptions);
+        qualityDropdown.RefreshShownValue();
+        qualityDropdown.onValueChanged.AddListener(SetQuality);
+    }
+
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ResolutionIndex", resolutionDropdown.value);
+        PlayerPrefs.SetInt("Fullscreen", Screen.fullScreen ? 1 : 0);
+        PlayerPrefs.SetInt("QualityLevel", QualitySettings.GetQualityLevel());
+        PlayerPrefs.SetInt("VSync", QualitySettings.vSyncCount > 0 ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
